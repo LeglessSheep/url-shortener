4f12e5237409da7f93bea524f2fe5c94a508ea12
Lucas <lucas_ffl@hotmail.com>
2020-03-02 16:01:21 -0300

    presetting configurations

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const express = require('express')
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
 const port = 3000;
+const server = express();
 
+let routes = require('./routes');
 
-const app = express()
+// Server settings - Will config later
+// server.set('env', config.env);
+// server.set('port', config.port);
+// server.set('hostname', config.hostname);
+// server.set('viewDir', config.viewDir);
 
-app.get('/', (req, res) => res.send('Hello World2'))
+// Returns middleware that parses json
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(logger('dev'));
 
-app.listen(port)
+// Set up routes
+routes.init(server);
+
+let hostname = server.get('hostname');
+
+server.listen(port, function() {
+  console.log('Express server listening on - http://' + hostname + ':' + port);
+});
+
+server.get('/', (req, res) => res.send('Hello World2'));
+server.listen(port);
